8dca89798f4c737ab3c575330554f0c64ba5c354
Michael Kuo <tylpk1216@gmail.com>
2020-06-02 10:18:17 +0800

    Fix some passwords are not tried issue.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 def connect_to_diagnosis(ttyName):
     myserial = None
     try:
-        ser = serial.Serial(ttyName, 115200, timeout=1)
+        ser = serial.Serial(ttyName, 115200, timeout=2)
         # ser.open()
         if not ser.is_open:
             raise BaseException
@@ -14,29 +14,48 @@ def connect_to_diagnosis(ttyName):
         print(ttyName, str(e))
     return myserial
 
+# 0 - com port issue, retry.
+# 1 - password is wrong.
+# 2 - password is right.
 def diagnosis_login(myserial, username, password):
     try:
         myserial.write(b'\n')  # poke
         resp = myserial.read(50)
         dbg_print(resp)
+
         if b'login' in resp:
             dbg_print('Entering username {}'.format(username))
             myserial.write(username.encode() + b'\n')
             resp = myserial.read(50)
             dbg_print(resp)
-        if b'Password' in resp:
-            dbg_print('Entering password {}'.format(password))
-            myserial.write(password.encode() + b'\n')
-            resp = myserial.read(80)
-            dbg_print(resp)
+            if b'Password' in resp:
+                dbg_print('Entering password {}'.format(password))
+                myserial.write(password.encode() + b'\n')
+                resp = myserial.read(80)
+                dbg_print(resp)
+
+                if b'# ' in resp:
+                    return 2
+                elif b'login' in resp:
+                    return 1
+                else:
+                    # result is not back
+                    resp = myserial.read(80)
+                    dbg_print(resp)
+            else:
+                return 0
+        else:
+            return 0
+
     except serial.SerialTimeoutException as e:
         err_print('Timeout: {}'.format(e))
+        return 0
     except BaseException as e:
         err_print(str(e))
-        return False
+        return 0
     if b'# ' in resp:
-        return True
-    return False
+        return 2
+    return 1
 
 def err_print(content):
     print("[error] {}".format(content))
@@ -60,9 +79,20 @@ def main():
     lines = f.readlines()
     f.close()
 
+    i = 1
     for line in lines:
-        res = diagnosis_login(myserial, account, line)
-        if res == True:
+        # remove \r\n
+        line = line.replace('\r', '')
+        line = line.replace('\n', '')
+
+        print(i, line)
+        i = i + 1
+
+        res = 0
+        while res == 0:
+            res = diagnosis_login(myserial, account, line)
+
+        if res == 2:
             print('\r\n\r\n')
             print('--------------------')
             print('Password is ', line)
